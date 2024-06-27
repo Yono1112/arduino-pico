5dcd4352a9da8ae2d451eefc72d0cea7063af4de
Yono1112 <yuma.ohno.sss@gmail.com>
2024-06-27 20:34:42 +0900

    feat: modify code to test DHT22

--- pi-pico-community-core.ino
+++ pi-pico-community-core.ino
@@ -1,17 +1,82 @@
-#include <Servo.h>
+#include <DHT.h>
 
-Servo myservo;  // create servo object to control a servo
+// DHT22のDATAピンはデジタルピン7に接続
+#define DHTPIN 28
+// 接続するセンサの型番
+#define DHTTYPE DHT22
 
-int pos = 0;    // variable to store the servo position
+// センサーの初期化
+DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
-  Serial1.begin(115200);
-  Serial1.println("Hello, Raspberry Pi Pico!");
-  myservo.attach(9);
+// 測定結果の統計データを保存するための構造体
+struct {
+  uint32_t total;
+  uint32_t ok;
+  uint32_t crc_error;
+  uint32_t time_out;
+  uint32_t connect;
+  uint32_t ack_l;
+  uint32_t ack_h;
+  uint32_t unknown;
+} stat = { 0, 0, 0, 0, 0, 0, 0, 0};
+
+void setup()
+{
+    Serial1.begin(115200);
+    dht.begin();
+    Serial1.println("Start Raspberry Pi Pico DHT22 Test!");
+    Serial1.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)\tTime (us)");
 }
 
-void loop() {
-  myservo.write(0);
-  delay(500);
-  myservo.write(90);
+void loop()
+{
+    delay(3000);
+
+    Serial1.print("DHT22, \t");
+
+    uint32_t start = micros();
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    uint32_t stop = micros();
+
+    stat.total++;
+
+    if (isnan(h) || isnan(t))
+    {
+        Serial1.print("Failed to read from DHT sensor!\t");
+        stat.unknown++;
+    }
+    else
+    {
+        stat.ok++;
+        Serial1.print("OK,\t");
+        Serial1.print(h, 1);
+        Serial1.print(",\t");
+        Serial1.print(t, 1);
+        Serial1.print(",\t");
+        Serial1.print(stop - start);
+        Serial1.println();
+    }
+
+    if (stat.total % 10 == 0)
+    {
+        Serial1.println("\nTOT\tOK\tCRC\tTO\tCON\tACK_L\tACK_H\tUNK");
+        Serial1.print(stat.total);
+        Serial1.print("\t");
+        Serial1.print(stat.ok);
+        Serial1.print("\t");
+        Serial1.print(stat.crc_error);
+        Serial1.print("\t");
+        Serial1.print(stat.time_out);
+        Serial1.print("\t");
+        Serial1.print(stat.connect);
+        Serial1.print("\t");
+        Serial1.print(stat.ack_l);
+        Serial1.print("\t");
+        Serial1.print(stat.ack_h);
+        Serial1.print("\t");
+        Serial1.print(stat.unknown);
+        Serial1.println("\n");
+        Serial1.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)\tTime (us)");
+    }
 }
